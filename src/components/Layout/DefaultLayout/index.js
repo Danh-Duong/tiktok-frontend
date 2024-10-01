e06e8f754fa7789b8b139e699e8f2b7fdca4f405
NgocDanh <duongngocdanh6@gmail.com>
2024-10-01 09:44:22 +0700

    config route and create Layout

---
+++ src/components/Layout/DefaultLayout/index.js
@@ -0,0 +1,17 @@
+// import Header from "../components/Header";
+import Header from "~/components/Layout/components/Header"
+import Sidebar from "./Sidebar";
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                {children}
+            </div>
+        </div >
+    );
+}
+
+export default DefaultLayout;
